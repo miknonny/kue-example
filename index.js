4fb981ba0948058a890536e6f2e5cfe636f55cdc
nonnycode <miknonny@gmail.com>
2016-04-27 12:58:43 +0100

    added remove job on completed and createjob

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const queue = kue.createQueue()
 // one minute
 const minute = 60000
 
-var job
+// Create Job function.
 const createJob = () => {
-  job = queue.create('email', {
+    queue.create('email', {
     title: 'Account renewal required',
     to: 'miknonny@gmail.com',
     template: 'renewal-email'
@@ -16,21 +16,22 @@ const createJob = () => {
     .save()
 }
 
+// Invoking the create job function.
 createJob()
 
+// Remove the job on completed and create job again.
 queue.on('job complete', (id, result) => {
+  kue.Job.get(id, (err, job) => {
+    if (err) return;
+    job.remove(err => {
+      if (err) throw err;
+      console.log(`removed completed job ${job.id}`)
+    })
+  })
   createJob()
 })
 
-// queue.create('email', {
-//   title: 'Account expired',
-//   to: 'miknonny@gmail.com',
-//   template: 'expired-email'
-// }).delay(10000)
-//   .priority('high')
-//   .save()
-
-// Once process to handle both email jobs.
+// queue.process will always be called to handle jobs.
 queue.process('email', 10, (job, done) => {
   console.log(job.data)
   setTimeout(() => {
@@ -38,6 +39,6 @@ queue.process('email', 10, (job, done) => {
   }, Math.random() * 10000)
 })
 
-// Starting the UI.
+// Starting Kue UI.
 kue.app.listen(3000)
 console.log('UI started on port 3000')
